38cd3267e4fb2c984f8386627747a46a2ecd53fc
Egor <pea321@yandex.ru>
2022-10-14 14:07:21 +0300

    changed

--- yatube/settings.py
+++ yatube/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = '7(k#0+x*uk(80wsa8ce(5bc(g-cuh%o7u&o19xgt_%^)^^j%c%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        "localhost",
+        "127.0.0.1",
+        "[::1]",
+        "testserver",
+    ]
 
 
 # Application definition
